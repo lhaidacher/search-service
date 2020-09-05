c4e0eb58f0250f69c1f9d975fbc832c06bbb1b19
Lukas Haidacher <lukas.haidacher@icloud.com>
2020-09-05 22:49:23 +0200

    added failing test

--- src/test/java/at/fhj/swd/searchservice/SearchServiceApplicationTests.java
+++ src/test/java/at/fhj/swd/searchservice/SearchServiceApplicationTests.java
@@ -1,8 +1,10 @@
 package at.fhj.swd.searchservice;
 
 import at.fhj.swd.searchservice.service.impl.SearchServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SearchServiceApplicationTests {
@@ -20,6 +22,11 @@ class SearchServiceApplicationTests {
     @Test
     public void foobar() {
         new SearchServiceImpl().getTrendingKeywords();
+    }
+
+    @Test
+    public void barfoo() {
+        Assertions.fail();
     }
 
 }
